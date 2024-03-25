eddeed390f20bdd7d15b4fb740ab07ab7051bee8
LENOVO <test23062000@gmail.com>
2024-03-25 10:57:25 +0300

    dzz

---
+++ app/src/main/java/com/example/android1homework5group16/Car.kt
@@ -0,0 +1,8 @@
+package com.example.android1homework5group16
+
+data class Car(
+    val nameCars: String,
+    val carImage: Int,
+    val wheels: Int,
+    val speed: Int
+)
--- app/src/main/java/com/example/android1homework5group16/SecondActivity.kt
+++ app/src/main/java/com/example/android1homework5group16/SecondActivity.kt
@@ -5,10 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.android1homework5group16.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySecondBinding
+    private val carsListAdapter = CarsListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fillCarsList()
+        setupRecyclerView()
         }
+
+    private fun fillCarsList() {
+        val carsList = listOf(
+            Car("БмВ", R.drawable.img1,4,210),
+            Car("Ламборгини", R.drawable.img2,4,300),
+            Car("Порша", R.drawable.img3,4,220),
+            Car("Волва", R.drawable.img4,4,100),
+            Car("Мерседес", R.drawable.img5,4,280),
+            Car("Формула1", R.drawable.img6,4,300),
+            Car("Мустанг", R.drawable.img7,4,200),
+            Car("Банана", R.drawable.img8,4,110),
+        )
+        carsListAdapter.setData(carsList)
     }
+
+
+    private fun setupRecyclerView() {
+        binding.rvCar.adapter = carsListAdapter
+    }
+}
